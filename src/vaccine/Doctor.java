160a101dd9009f90ff805210b313bd79bd29ca6d
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-08-16 21:10:24 +0530

    Initial commit

---
+++ src/vaccine/Doctor.java
@@ -0,0 +1,12 @@
+package vaccine;
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class Doctor
+{
+	public String doctorName;
+	public String doctorType;
+	public String doctorAvailableDay;
+	public Map<String,Boolean> availabilty=new LinkedHashMap<>();
+}
