f7e4ed2991efc2e7393138a8b832e347c7d0ae52
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-08-28 19:21:36 +0530

    Add comments to admin

--- src/vaccine/Admin.java
+++ src/vaccine/Admin.java
@@ -1,16 +1,18 @@
 package vaccine;
 
-public class Admin 
+public class Admin
 {
+	//Admin predefined login details
 	private String adminID = "admin";
-	private String password = "123";
-	
+	private String password = "admin";
+
 	Admin(String adminId,String password)
 	{
 		this.adminID = adminId;
 		this.password = password;
 	}
-	
+
+	//Admin validation part
 	public boolean validateAdmin(String adminId,String password)
 	{
 		if(this.adminID.contentEquals(adminId)&&this.password.equals(password))
